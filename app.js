bd3a68325fddcc6040c2b9cf2c0e5ad5027b3a26
sardor <rovshonicon@gmail.com>
2022-05-07 23:17:06 +0500

    bitdi
    bitdi

--- app.js
+++ app.js
@@ -1,43 +1,84 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const btnX = document.querySelector('.btnX'),
-        title = document.querySelector('#title'),
-        sum = document.querySelector('#author'),
-        value_container = document.querySelector('.value-container'),
-        book = document.querySelector('#book-list')
+window.addEventListener("DOMContentLoaded", () => {
+  const btnX = document.querySelector(".btnX"),
+    title = document.querySelector("#title"),
+    sum = document.querySelector("#author"),
+    value_container = document.querySelector(".value-container"),
+    book = document.querySelector("#book-list");
 
-        
-    let count = 1
-    let full = 0
-    btnX.addEventListener('click', (e) => {
-        e.preventDefault()
-        if (title.value == '' || sum.value == '') {
-            alert('enter your changes')
-        }else {
-            const newRow = document.createElement('tr')
+  let data = JSON.parse(localStorage.getItem("myCar")) || [];
 
-            const newNum = document.createElement('th')
-            newNum.innerHTML = count++
-            newRow.appendChild(newNum)  
+  let count = 1;
+  let full = 0;
+  let total = "total : ";
 
-            const newTitle = document.createElement('th')
-            newTitle.innerHTML = title.value
-            newRow.appendChild(newTitle)
+  if (data.length) {
+    console.log(data);
+    data.forEach((i) => {
+      const newRow = document.createElement("tr");
+      const newNum = document.createElement("th");
+      const newTitle = document.createElement("th");
+      const newCar = document.createElement("th");
+      const new_count = document.createElement("div");
+      newNum.innerHTML = i.num;
+      newRow.appendChild(newNum);
 
-            const newCar = document.createElement('th')
-            newCar.innerHTML = sum.value
-            newRow.appendChild(newCar)                
-            
-            const new_count = document.createElement("div")
-            full += Number(sum.value)
-            value_container.innerHTML = full
-            value_container.appendChild(new_count)
+      newTitle.innerHTML = i.title;
+      newRow.appendChild(newTitle);
 
-            book.appendChild(newRow)
-            
-        }
+      newCar.innerHTML = i.sum;
+      newRow.appendChild(newCar);
 
-        title.value = ''
-        sum.value = ''
-    })
-    
-})
+      value_container.innerHTML = total + i.full;
+      value_container.appendChild(new_count);
+
+      book.appendChild(newRow);
+
+      count = i.num;
+      full = i.full;
+    });
+    count++;
+  }
+
+  btnX.addEventListener("click", (e) => {
+    if (title.value == "" || sum.value == "") {
+      return alert("enter your changes");
+    }
+    e.preventDefault();
+    let obj = {
+      num: 0,
+      title: "",
+      sum: "",
+      full: 0,
+    };
+    const newRow = document.createElement("tr");
+    const newNum = document.createElement("th");
+    const newTitle = document.createElement("th");
+    const newCar = document.createElement("th");
+    const new_count = document.createElement("div");
+
+    obj.num = count;
+    newNum.innerHTML = count++;
+    newRow.appendChild(newNum);
+
+    obj.title = title.value.toUpperCase();
+
+    newTitle.innerHTML = title.value.toUpperCase();
+    newRow.appendChild(newTitle);
+
+    obj.sum = sum.value;
+
+    newCar.innerHTML = sum.value;
+    newRow.appendChild(newCar);
+
+    full += Number(sum.value);
+    obj.full = full;
+    value_container.innerHTML = total + full +  'sum';
+
+    value_container.appendChild(new_count);
+    book.appendChild(newRow);
+    data.push(obj);
+    localStorage.setItem("myCar", JSON.stringify(data));
+    title.value = "";
+    sum.value = "";
+  });
+});
